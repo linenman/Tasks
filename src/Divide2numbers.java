7844a2a3a58642d019285c54e64df67c53e03938
Emre_Cydeo <emre.ketenci34@gmail.com>
2023-05-19 13:33:14 -0400

    created second task

---
+++ src/Divide2numbers.java
@@ -0,0 +1,44 @@
+public class Divide2numbers {
+    public static void main(String[] args) {
+
+
+        double a = 50;
+        double b = 5;
+
+        double count = 0;
+
+        while (a>=b){
+            a-=b;
+            count++;
+
+
+        }
+        System.out.println(count);
+    }
+}
+
+
+/*
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 9;
+
+        int count = 0;
+
+        while (a >= b) {
+            a -= b;
+            count++;
+        }
+
+        System.out.println(count+" with a remainder of "+a);
+
+
+    }
+
+
+}
+
+/*
+Create a class named DivideTwoNumbers and Write a program that can divide two
+positive numbers without using / (division) and * (multiplication) operators.
+ */
